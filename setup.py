9057607296435bb8d21f10edaf9bb1b5b7663a03
qiansuyu319 <qiansuyu319@users.noreply.github.com>
2025-09-01 23:12:32 +0800

    chore: set author to qiansuyu319 in metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(                                                                                                         
     name="gazelle",                                                                                                  
     version="0.0.1",                                                                                                      
-    author="Fiona Ryan",                                                                                                                                                                              
+    author="qiansuyu319",                                                                                                                                                                               
     description="Gaze-LLE: Gaze Target Estimation via Large-Scale Learned Encoders",    
     packages=setuptools.find_packages()                                                                             
-)                                                                                                                               
+)
